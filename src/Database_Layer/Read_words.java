181f6c42faa4c3abb1601b835c0821628ab55a15
Abubakar133 <116784748+Abubakar133@users.noreply.github.com>
2022-12-10 03:42:27 +0500

    Creating function for reading the words from database

--- src/Database_Layer/Read_words.java
+++ src/Database_Layer/Read_words.java
@@ -1,5 +1,44 @@
 package Database_Layer;
 
-public class Read_words {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JOptionPane;
+
+public class Read_words {
+	static List<String> A_Str = new ArrayList<String>();
+
+	public static void Word() {
+
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker", "root", "");
+
+			if (!con.isClosed()) {
+				PreparedStatement ps = con.prepareStatement("SELECT * from word");
+
+				ResultSet rs = ps.executeQuery();
+
+				while (rs.next()) {
+					int id = (int) rs.getObject("word_id");
+					int fre = (int) rs.getObject("frequency");
+					A_Str.add(Integer.toString(id));
+					A_Str.add((String) rs.getObject("words"));
+					A_Str.add(Integer.toString(fre));
+
+				}
+
+				con.close();
+				JOptionPane.showMessageDialog(null, "FIle Retrived Successfully please wait....");
+			} else
+				JOptionPane.showMessageDialog(null, "Connection Not Found");
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Driver Error Found");
+		}
+
+	}
 }
