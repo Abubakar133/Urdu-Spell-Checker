f49f07567d63bb01caf76475c71860e43068902d
Abubakar133 <116784748+Abubakar133@users.noreply.github.com>
2022-12-10 03:39:21 +0500

    Making functions for Update and delete

--- src/BussinessLogic_Layer/view_update.java
+++ src/BussinessLogic_Layer/view_update.java
@@ -1,9 +1,29 @@
 package BussinessLogic_Layer;
 
+import java.util.List;
+
 import Database_Layer.DatabaseWord;
 import Database_Layer.Read_words;
 
 public class view_update {
-	Read_words obj=new Read_words();
-	DatabaseWord obj2=new DatabaseWord();
+	Read_words obj = new Read_words();
+	DatabaseWord obj2 = new DatabaseWord();
+
+	public List getWords() {
+		obj.Word();
+
+		return obj.returnword();
+
+	}
+
+	public void update(String word, int id, int fre) {
+
+		obj2.update(word, id, fre);
+
+	}
+
+	public void delete(int id) {
+
+		obj2.delete(id);
+	}
 }
