082658a876e251d5712ef0fbd4a01fdafef41d6a
Abubakar133 <116784748+Abubakar133@users.noreply.github.com>
2022-12-10 03:48:30 +0500

    Writing the function for updating the word

--- src/Database_Layer/DatabaseWord.java
+++ src/Database_Layer/DatabaseWord.java
@@ -12,59 +12,91 @@ import javax.swing.JOptionPane;
 import BussinessLogic_Layer.Make_Word;
 
 public class DatabaseWord {
-	public DatabaseWord(){
-		
-		
-	}
-	static boolean t=false;
-	static List<String> word_DB2=new ArrayList<String>();
-public static void Word_Data() {
-	Make_Word obj=new Make_Word();
-	List<String> word_DB=word_DB=obj.Word_List();
-	List<Integer> word_Fre=word_Fre=obj.Word_Fre();
-	word_DB2=word_DB;
-		
-		
-		
-    	
-    	try {
-        	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker","root","");
-        	
-        	PreparedStatement st1 = con.prepareStatement("TRUNCATE TABLE word");
-	        st1.execute();
-        	
-        		for(int i=0;i<word_DB.size();i++) {
-        		int Fre=(int) word_Fre.get(i);
-        		
-    	        PreparedStatement st = con.prepareStatement("insert into word values("+null+",'"+word_DB.get(i)+"',"+Fre+")");
-    	        st.execute();
-        		
-        		}
-        		JOptionPane.showMessageDialog(null,"Word Sent Successfully in Database");
-        		t=true;
-        	con.close();
-        }
-        catch(SQLException e) {
-        	JOptionPane.showMessageDialog(null,"Connection Not Found");
-        }
-		
-		
+	public DatabaseWord() {
+
 	}
 
-    public  List<String> Test() {
-	
-	      return word_DB2; 
-        }
-public static boolean CheckConnection() {
-		
-	Word_Data();
-		if(t==true) {
-			return true;
+	static boolean t = false;
+	static List<String> word_DB2 = new ArrayList<String>();
+
+	public static void Word_Data() {
+		Make_Word obj = new Make_Word();
+		List<String> word_DB = word_DB = obj.Word_List();
+		List<Integer> word_Fre = word_Fre = obj.Word_Fre();
+		word_DB2 = word_DB;
+
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker", "root", "");
+
+			PreparedStatement st1 = con.prepareStatement("TRUNCATE TABLE word");
+			st1.execute();
+
+			for (int i = 0; i < word_DB.size(); i++) {
+				int Fre = (int) word_Fre.get(i);
+
+				PreparedStatement st = con
+						.prepareStatement("insert into word values(" + null + ",'" + word_DB.get(i) + "'," + Fre + ")");
+				st.execute();
+
+			}
+			JOptionPane.showMessageDialog(null, "Word Sent Successfully in Database");
+			t = true;
+			con.close();
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Connection Not Found");
 		}
-		else {
+
+	}
+
+	public List<String> Test() {
+
+		return word_DB2;
+	}
+
+	public static boolean CheckConnection() {
+
+		Word_Data();
+		if (t == true) {
+			return true;
+		} else {
 			return false;
 		}
-		
-		
+
 	}
+
+	public void delete(int id) {
+
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker", "root", "");
+
+			String query = "DELETE FROM word WHERE word_id = '" + id + "'";
+			PreparedStatement st1 = con.prepareStatement(query);
+			st1.execute();
+
+			JOptionPane.showMessageDialog(null, "Word Delete Successfully in Database");
+
+			con.close();
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Connection Not Found");
+		}
+	}
+
+	public void update(String word, int id, int fre) {
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker", "root", "");
+
+			String query = "UPDATE word SET word_id = " + id + ", words = '" + word + "', frequency = " + fre
+					+ " WHERE word_id like '" + id + "'";
+
+			PreparedStatement st1 = con.prepareStatement(query);
+			st1.execute();
+
+			JOptionPane.showMessageDialog(null, "Word update Successfully in Database");
+
+			con.close();
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Connection Not Found");
+		}
+	}
+
 }
