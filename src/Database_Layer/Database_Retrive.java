18bcfd93474244bd8a4ba6abd182152c75da3dc5
Abubakar133 <116784748+Abubakar133@users.noreply.github.com>
2022-12-10 15:11:21 +0500

    Remove Deadl code

--- src/Database_Layer/Database_Retrive.java
+++ src/Database_Layer/Database_Retrive.java
@@ -11,56 +11,48 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 public class Database_Retrive {
-	public Database_Retrive(){
-		
-		
+	public Database_Retrive() {
+
 	}
-	 
-	static boolean t=false;
+
+	static boolean t = false;
 
 	public static List<String> Word() {
-		 List<String> A_Str = new ArrayList<String>();
+		List<String> A_Str = new ArrayList<String>();
 		try {
-	    	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker","root","");
-	    	
-	    	if (!con.isClosed())
-	    	   {
-	    		PreparedStatement ps = con.prepareStatement("SELECT * from Paragraph");
-	          
-	            ResultSet rs = ps.executeQuery();
-	    	      
-	    	      while (rs.next())
-	    	      {
-	    	    	 
-	    	        A_Str.add((String) rs.getObject("Context"));
-	    	        
-	    	        
-	    	      }
-                t=true;
-	    	      con.close();
-	    	      JOptionPane.showMessageDialog(null,"FIle Retrived Successfully please wait....");
-	    	   }
-	    	   else
-	    		   JOptionPane.showMessageDialog(null,"Connection Not Found");
-	    	}
-	    catch(SQLException e) {
-	    	JOptionPane.showMessageDialog(null,"Driver Error Found");
-	    }
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker", "root", "");
+
+			if (!con.isClosed()) {
+				PreparedStatement ps = con.prepareStatement("SELECT * from Paragraph");
+
+				ResultSet rs = ps.executeQuery();
+
+				while (rs.next()) {
+
+					A_Str.add((String) rs.getObject("Context"));
+
+				}
+				t = true;
+				con.close();
+				JOptionPane.showMessageDialog(null, "FIle Retrived Successfully please wait....");
+			} else
+				JOptionPane.showMessageDialog(null, "Connection Not Found");
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Driver Error Found");
+		}
 
 		return A_Str;
 	}
 
 	public static boolean CheckConnection() {
-		
+
 		Word();
-		if(t==true) {
+		if (t == true) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
-		
-		
+
 	}
-	
+
 }
